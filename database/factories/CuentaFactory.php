b0f1f3f2465127b3e794136eb3cfb68b28183d40
JesusPuga <jesuspuga01@gmail.com>
2018-11-19 00:54:28 -0600

    Created factories and seeders

--- database/factories/CuentaFactory.php
+++ database/factories/CuentaFactory.php
@@ -4,6 +4,11 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Cuenta::class, function (Faker $faker) {
     return [
-        //
+        'saldo' => rand(20000,50000),
+        'fecha_apertura' => $faker->date.past(5),
+        'fecha_cierre' => $faker->date.future(5),
+        'slug' => str_lug($faker->text(124)),
+        'credito' => rand(20000,50000),
+        'debito' => rand(20000,50000),
     ];
 });
--- database/migrations/2018_11_18_225339_create_clientes_table.php
+++ database/migrations/2018_11_18_225339_create_clientes_table.php
@@ -15,7 +15,16 @@ class CreateClientesTable extends Migration
     {
         Schema::create('clientes', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('cuenta_id')->unsigned();
+            $table->string('slug',128)->unique();
+            $table->string('cuenta',124);
+            $table->string('telefono',16);
             $table->timestamps();
+
+            //relation
+            $table->foreign('cuenta_id')->references('id')->on('cuentas')
+            ->onDelete('cascade')
+            ->onUpdate('cascade');
         });
     }
 
